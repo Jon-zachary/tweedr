7c4a963d82c467a151ff26aa9c3bd15468786ed2
Jon Zachary <jontzachary@gmail.com>
2017-05-21 17:29:46 -0400

    add cors to app.js

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ var api = require('./routes/api');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
+var cors = requires('cors');
 var db = require('./db/queries');
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
